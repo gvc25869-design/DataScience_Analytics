b653153e5ed2e0bf788a9d7695a9003b956aeaf9
gvc25869-design <gvc25869@gmail.com>
2025-10-09 16:43:55 +0530

    Done

---
+++ ModularApproach/demo.py
@@ -0,0 +1,9 @@
+def sum(num1,num2):         # Function Definition
+    num3 = num1+num2        # Expresssion
+    return num3             # returns the value num3
+# testing 
+'''
+ans = sum(100,200)
+print(ans)
+'''
+    
